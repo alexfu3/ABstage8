62c4f3fc171753a8d1d12bf5542f12811634d284
alexfu3 <afu20@mtlstudents.net>
2020-11-01 20:00:18 -0500

    BG and score

--- sketch.js
+++ sketch.js
@@ -9,9 +9,10 @@ var backgroundImg,platform;
 var bird, slingshot;
 
 var gameState = "onSling";
+var score = 0;
 
 function preload() {
-    backgroundImg = loadImage("sprites/bg.png");
+    getBackgroundImg();
 }
 
 function setup(){
@@ -45,18 +46,24 @@ function setup(){
 }
 
 function draw(){
-    background(backgroundImg);
+    if(backgroundImg){
+        background(backgroundImg);
+    }
+    textSize(30);
+   text("Score:"+score,width-300,50);
     Engine.update(engine);
     //strokeWeight(4);
     box1.display();
     box2.display();
     ground.display();
     pig1.display();
+    pig1.score();
     log1.display();
 
     box3.display();
     box4.display();
     pig3.display();
+    pig3.score();
     log3.display();
 
     box5.display();
@@ -67,6 +74,7 @@ function draw(){
     platform.display();
     //log6.display();
     slingshot.display();    
+
 }
 
 function mouseDragged(){
@@ -86,3 +94,36 @@ function keyPressed(){
        // slingshot.attach(bird.body);
     }
 }
+
+
+async function getBackgroundImg(){
+
+    var response = await fetch("https://worldtimeapi.org/api/timezone/America/New_York");
+    var responseJSON = await response.json();
+    //console.log(responseJSON.datetime);
+    var datetime = responseJSON.datetime;
+    var hour = datetime.slice(11,13);
+    console.log(hour);
+
+
+
+    if(hour>=06 && hour<=19){
+        bg = "sprites/bg.png"
+    }
+    else{
+        bg = "sprites/bg2.jpg"
+    }
+    backgroundImg = loadImage(bg);
+    console.log(backgroundImg);
+}
+/*
+JSON - Data structure that helps store multiple values
+ at the same time in one variable inside { }
+
+ JSON - Javascript Object Notation
+
+
+
+
+
+*/
